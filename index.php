262bb21ca6614294fc1b1e69461195aeb87a38c0
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2020-01-16 00:58:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,388 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="bootstrap\4.4.1\css\bootstrap.min.css">
+
+	<link rel="stylesheet" href="fontawesome/css/all.css" >
+	<link rel="stylesheet" href="css\estilo.css">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="bootstrap/4.4.1/js/bootstrap.min.js"></script>	
+	
+	<!--*********************************** SELECT COUNTRY ************************************************-->
+	<link rel="stylesheet" href="//unpkg.com/bootstrap-select@1.12.4/dist/css/bootstrap-select.min.css" type="text/css" />
+	<link rel="stylesheet" href="//unpkg.com/bootstrap-select-country@4.0.0/dist/css/bootstrap-select-country.min.css" type="text/css" />
+
+	<script src="//unpkg.com/bootstrap-select@1.12.4/dist/js/bootstrap-select.min.js"></script>
+	<script src="//unpkg.com/bootstrap-select-country@4.0.0/dist/js/bootstrap-select-country.min.js"></script>
+	<!--********************************** SELECT COUNTRY ************************************************-->
+	
+  
+
+
+
+<title>XPTO Company</title>
+</head>
+
+<body>
+
+<div class="container-fluid">
+	<nav class="navbar navbar-expand-lg p-3 bg-light shadow border-pill navbar-light" style="z-index:1;border-bottom-left-radius:25px;border-bottom-right-radius:25px;">
+		<a class="navbar-brand" href="#">
+			<img src="imagens/logo_xpto_color.png" class="img-fluid"  style="height:30px;" alt="XPTO Company">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+			<ul class="navbar-nav nav-fill w-50 align-items-center">
+				<li class="nav-item">
+					<a class="nav-link" href="#m_servicos">Serviços</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Orçamentos</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contato</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Login</a>
+				</li>   
+				<div class="text-left">
+					<span style="font-size:7pt;color:silver;font-weight:bold;">Comece por aqui</span><br>
+					<button class="btn btn-sm btn-success" style="height:35px;width:130px;font-size:9pt;border-bottom-left-radius:20px;border-bottom-right-radius:25px;border-top-right-radius:25px;" >
+						<b>Getting Started</b>
+					</button>
+				</div>
+			</ul>		
+		</div>  
+	</nav>
+	<div class="row container-fluid " style="z-index:0;">
+		<div class="col-md-8 p-5" >
+			<iframe width="100%" height="500px" src="https://www.youtube.com/embed/iX40b0PdRfQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<h4>Apresentação da empresa</h4>
+			<h6>Fique por dentro de tudo em nossas redes sociais:</h6>
+			<div class="inline">
+				<span class="fab fa-youtube mr-3"></span>
+				<span class="fab fa-instagram mr-3"></span>
+				<span class="fab fa-linkedin mr-3"></span>
+				<span class="fab fa-facebook-square mr-3"></span>
+				<span class="fab fa-whatsapp mr-3"></span>
+			</div>
+		</div>
+				
+		<div class="col-md-4 p-5  container-fluid" style="background-color:rgba(247,248,248,0.2);">
+			<div class="pr-5 pt-2 p-2" style="z-index:2;font-weight:bold;font-family:Open Sans Extrabold;text-align:right;">
+				<h1>A <span class="fluid" style="color:#0b582c;">CONSULTORIA</span> que<br>
+				Você Mais Precisa</h1>
+			</div>
+			<div class="card-deck row m-3 p-0 border-top border-bottom container-fluid align-items-center"  id="btn_front_right">
+				<div class="row no-gutters bg-light rounded col m-2 p-2">
+					<div class="col-3 mt-2 pl-2">
+						<span class="fas fa-check "></span>
+					</div>
+					<div class="col-9 pl-2">
+						Análise de Processos
+					</div>
+				</div>
+				<div class="row no-gutters bg-light rounded col m-2 p-2">
+					<div class=" col-3 mt-2 pl-2">
+						<span class="card-link fas fa-check"></span>
+					</div>
+					<div class="col-9 pl-2">
+						Dedução e solução ágil
+					</div>
+				</div>
+				<div class="row no-gutters bg-light rounded col m-2 p-2">
+					<div class="col-3 mt-2 pl-2">
+						<span class="card-link fas fa-check"></span>
+					</div>
+					<div class="col-9 pl-2">
+						Aplicabilidade geral
+					</div>
+				</div>
+			</div>
+			
+			<div class="row mt-5 mb-5">
+				<ul style="font-weight:bold;">
+					<li>Atuação em Todo o Vale do Paraíba</li>
+					<li>+20 Anos de atendimento Full Time</li>
+				</ul>								
+			</div>
+			
+			<div class="row mb-5">
+				<h5 class="w-75"><b>Mais de 10 empresas na região estão com nossa acessoria ativa</b></h5>
+				<img class="w-75 img-fluid align-center" src="imagens/empresas_ribbon.png" alt="Empresas com nossa acessoria ativa">
+			</div
+			
+			<div class="row">
+				<div class="card-body rounded" style="background-color:rgba(156,229,183,0.5);">
+					<h6><b>Para quaiquer dúvidas a repeito do contranto, acesse nossa página de orçamentos para sanar todas eventuais questões.</b></h6>
+				</div>
+			</div>
+		</div>
+		
+		
+	</div>
+	
+	<a name="m_servicos">
+	<div class="row m-5">	
+		<div class="container-fluid">
+		
+			<div class="card-header rounded-top text-center" style="background-color:white;border:0px;" id="servicos_header">
+				<h1 class="d-inline">Serviços da XPTO,</h1><b> o que exatamente fazemos?</b>
+			</div>
+			<div class="card-group" id="servicos">		
+				<div class="card"> 
+					<div class="card-body">
+						<h5 class="card-title" style="border-top:5px solid #1d262b;">Matriz de rastreabilidade</h5>
+						
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+					</div>
+				</div>
+				<div class="card"> 
+					<div class="card-body">
+						<h5 class="card-title" style="border-top:5px solid #1fbf76" >Dedução e Aplicação</h5>
+						
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+					</div>
+				</div>
+				<div class="card"> 
+					<div class="card-body">
+						<h5 class="card-title" style="border-top:5px solid #92f075">Implementação da Nossa Plataforma</h5>
+						
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+					</div>
+				</div>
+				<div class="card"> 
+					<div class="card-body">
+						<h5 class="card-title" style="border-top:5px solid #180f55">Acessoria extendida</h5>
+						
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+						<p>	Lorem ipsum dolor sit amet,
+							consectetuer adipiscing elit. Aenean
+							scelerisque metus eget sem. Suspendisse
+							fermentum. Proin diam augue, semper
+							vitae, varius et, viverra id, felis. Nam a
+							nunc. Mauris et dolor. Aliquam metus.
+							Integer risus velit, facilisis eget, viverra
+							et, venenatis id, leo. Nullam libero nunc,
+							tristique eget, laoreet eu, sagittis id, ante.
+							Donec sit amet enim. Mauris et pede.
+							Vestibulum non arcu a ante feugiat
+							vestibulum.
+					</div>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+	
+	<a name="m_orcamentos">
+	<div class="container-fluid m-0 p-0" style="background-color:#f7f8f8;">
+		<nav class="navbar mb-3" style="border-left:4px solid #1fbf76;" >
+			<h2 class="navbar-brand " style="color:#1fbf76;">Nossos Principais Orçamentos</h2>
+
+			<form class="form-inline">
+				<select class="form-control selectpicker countrypicker"  data-flag="true" data-countries="BR,US">
+					<option >(R$) Brazilian Real</option>
+					<option >(US$) US Dolar</option>
+				</select>
+				<script>
+					$('.countrypicker').countrypicker();
+				</script>
+			</form>
+		</nav>
+
+		
+		<div class="row p-5 m-0 " style="background-color:#EDF1F2;border-top:3px solid gray;">
+			<div class="card-deck mb-3 text-center">
+				<div class="card mb-4 shadow-sm">
+					<div class="card-header">
+						<h4 class="my-0 font-weight-normal">INTRO PLAN</h4>
+					</div>
+					<div class="card-body border-pill">
+						<h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mês</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+
+						</ul>
+						<button type="button" class="btn btn-lg btn-block btn-outline-primary">R$ 399,90 Mensais</button>
+					</div>
+				</div>
+				<div class="card mb-4 shadow-sm">
+					<div class="card-header">
+						<h4 class="my-0 font-weight-normal">STANDARD PLAN</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mês</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>						
+						</ul>
+						<button type="button" class="btn btn-lg btn-block btn-primary">R$ 1099,90 Mensais</button>
+					</div>
+				</div>
+				<div class="card mb-4 shadow-sm">
+					<div class="card-header">
+						<h4 class="my-0 font-weight-normal">MASTER PLAN</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mês</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>							
+						</ul>
+						<button type="button" class="btn btn-lg btn-block btn-primary">R$14.299,99 AA</button>
+					</div>
+				</div>
+				<div class="card mb-4 shadow-sm">
+					<div class="card-header">
+						<h4 class="my-0 font-weight-normal">PARTNER PLAN</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mês</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>
+							<li>Lorem ipsum dolor sit amet, consectetuer
+							adipiscing elit. Aenean scelerisque metus eget sem.
+							Suspendisse fermentum. Proin diam augue.</li>							
+						</ul>
+						<button type="button" class="btn btn-lg btn-block btn-primary">R$16.799,99 AA</button>
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid p-4" style="background-color:white;">
+				<h4>Alguma dúvida?</h4> Mande agora mesmo para sanar qualquer questão
+				<input type="text" class="form-control">
+				<button type="submit" class="btn btn-lg btn-block btn-success">Enviar</button>
+				
+			</div>
+		</div>
+	</div>
+
+</div>
+
+</body>
+
+
+</html>
